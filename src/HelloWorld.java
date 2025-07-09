3387808c3967155b13817d3083656ed81cfe7ba2
Your Name <you@example.com>
2025-07-09 11:25:02 +0200

    test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-//        String unusedVariable = "This is not used";
+        String unusedVariable = "This is not used";
         System.out.println("Hello, World!");
     }
 }
